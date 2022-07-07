0e7244d533aa3ed8d2eba9d804b4d92607c71270
Youssef El-Zanaty <youssef-el-zanaty@outlook.com>
2022-07-07 15:45:10 +0200

    create index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const app = express();
+
+require("dotenv").config();
+
+require("./Start/routes")(app);
+require("./Start/mongo")();
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, async () => {
+  console.log(`Server started on port localhost:${port}`);
+});
+
+module.exports = server;
