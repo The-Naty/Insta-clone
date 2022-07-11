aa82fcd43a2746993dc5d375133822003423b99e
Youssef El-Zanaty <youssef-el-zanaty@outlook.com>
2022-07-11 03:27:11 +0200

    auth

--- Routes/auth.js
+++ Routes/auth.js
@@ -4,40 +4,12 @@ const UserController = require("../Controllers/user");
 const auth = require("../Middlewares/auth");
 
 router.get("/:id", auth, UserController.getUserInfo);
-router.get("/", auth, admin, UserController.getAllUser);
 router.post(
   "/signup",
   RequestValidator.validate(UserValidator.createUser),
   UserController.createUser
 );
 router.post("/login", UserController.userLogin);
-router.put("/:id", auth, admin, UserController.updateUser);
-router.delete("/:id", auth, admin, UserController.deleteUser);
-router.get("/auth/google", UserController.getGoogleUser);
-router.get("/auth/google/callback", UserController.getGoogleUserCallback);
-router.get("/login/success", UserController.loginSuccess);
-
-router.get("/", function (req, res) {
-  res.render("index.ejs"); // load the index.ejs file
-});
-
-router.get(
-  "/auth/facebook",
-  passport.authenticate("facebook", {
-    scope: ["public_profile", "email"],
-  })
-);
-
-router.get(
-  "/auth/facebook/callback",
-  passport.authenticate("facebook", {
-    successRedirect: "/profile",
-    failureRedirect: "/login",
-  }),
-  function (req, res) {
-    if (req.isAuthenticated()) return next();
-    res.redirect("/");
-  }
-);
+router.put("/:id", auth, UserController.updateUser);
 
 module.exports = router;
