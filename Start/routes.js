a25d2ff70ff558e975680cc4887e79cd89426cda
Youssef El-Zanaty <youssef-el-zanaty@outlook.com>
2022-07-21 17:56:00 +0200

    Profile router

--- Start/routes.js
+++ Start/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const ProfileRouter = require("../Routes/profile");
 const AuthRouter = require("../Routes/auth");
 const UserRouter = require("../Routes/user");
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use("/profile", ProfileRouter);
   app.use("/auth", AuthRouter);
   app.use("/user", UserRouter);
 };
