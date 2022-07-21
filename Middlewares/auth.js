dc47f37d0e3d4a5a9483fe70b8148eb64c139ff5
Youssef El-Zanaty <youssef-el-zanaty@outlook.com>
2022-07-21 18:19:24 +0200

    Update auth
    Solved auth issue where bearer gets replaced before checking if the token is provided or not that leads to an error.

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -2,9 +2,10 @@ const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
   const jwtToken = req.header("Authorization");
-  token = jwtToken.replace("Bearer ", "");
 
-  if (!token) return res.status(401).send("Access denied. No token provided.");
+  if (!jwtToken)
+    return res.status(401).send("Access denied. No token provided.");
+  token = jwtToken.replace("Bearer ", "");
   try {
     const decoded = jwt.verify(token, process.env.jwtPrivateKey);
     req.user = decoded;
