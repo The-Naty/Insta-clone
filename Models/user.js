8adf7c6698630b2f4a1fe93bcbf05b8d41a6d1d7
Youssef El-Zanaty <youssef-el-zanaty@outlook.com>
2022-07-21 19:05:32 +0200

    Update user.js
    Added Create and Update Date to User Model.

--- Models/user.js
+++ Models/user.js
@@ -1,30 +1,38 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-    maxlength: 16,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      maxlength: 16,
+      unique: true,
+    },
+    nickName: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 16,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
   },
-  nickName: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 16,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign({ _id: this._id }, process.env.jwtPrivateKey);
