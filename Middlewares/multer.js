ed137cf545d59b6aca4bb53981de64553b19d42d
Youssef El-Zanaty <youssef-el-zanaty@outlook.com>
2022-07-26 02:24:46 +0200

    Update multer.js
    Multer configurations

--- Middlewares/multer.js
+++ Middlewares/multer.js
@@ -1,1 +1,13 @@
 const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: "../Public/Uploads",
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage }).single;
