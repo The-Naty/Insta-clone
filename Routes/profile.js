e02c7e5af629461ba53d70a67d8ea607ece273cb
Youssef El-Zanaty <youssef-el-zanaty@outlook.com>
2022-07-27 02:18:42 +0200

    export multer upload function

--- Routes/profile.js
+++ Routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ProfileController = require("../Controllers/profile");
 const auth = require("../Middlewares/auth");
+const upload = require("../Middlewares/multer");
 
 router.get("/me", auth, ProfileController.getMyInfo);
 router.get("/:id", auth, ProfileController.getUserInfo);
