cf239700e205f6d0c660e2ed42c4a8ad86b23024
The-Naty <youssef-el-zanaty@outlook.com>
2022-08-18 20:27:14 +0200

    Update post.js
    added multer middleware to post upload

--- Routes/post.js
+++ Routes/post.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const PostController = require("../Controllers/post");
 
 router.get("/", PostController.getAllPost);
+router.post(
+  "/upload",
+  [auth, upload.single("post")],
+  PostController.uploadPicture
+);
 // router.delete("/:id", PostController.deleteUser);
 
 module.exports = router;
