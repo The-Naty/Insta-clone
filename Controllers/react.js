485b615779a2470c515f7dcfd3983f32ec4809b6
The-Naty <youssef-el-zanaty@outlook.com>
2022-08-31 16:28:06 +0200

    Update react.js
    unfollow controller

--- Controllers/react.js
+++ Controllers/react.js
@@ -17,18 +17,18 @@ ReactController.followUser = async (req, res) => {
   }
 };
 
-// ReactController.unFollowUser = async (req, res) => {
-//   try {
-//     const respond = await ReactService.unFollowUser(
-//       req.user._id,
-//       req.body.userId
-//     );
+ReactController.unFollowUser = async (req, res) => {
+  try {
+    const respond = await ReactService.unFollowUser(
+      req.user._id,
+      req.body.userId
+    );
 
-//     res.status(200).send(respond);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send(error.message);
-//   }
-// };
+    res.status(200).send(respond);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+};
 
 module.exports = ReactController;
