ae8b189ce460ae1e5001aca723fd5485403fd1fc
The-Naty <youssef-el-zanaty@outlook.com>
2022-08-31 16:28:49 +0200

    Update react.js
    unfollow service

--- Services/react.js
+++ Services/react.js
@@ -30,29 +30,30 @@ ReactService.followUser = async (followerId, followedId) => {
   }
 };
 
-// ReactService.unFollowUser = async(followerId, followedId)=>{
-//   try {
-//     if (!followedId) return { error: "wrong user ID" };
-//     const followedUser = await User.findById(followedId).select("-password");
-//     const followerUser = await User.findById(followerId).select("-password");
+ReactService.unFollowUser = async (followerId, followedId) => {
+  try {
+    if (!followedId) return { error: "wrong user ID" };
+    const followedUser = await User.findById(followedId).select("-password");
+    const followerUser = await User.findById(followerId).select("-password");
 
-//     for (let i = 0; i < followedUser.follower.length; i++) {
-//       if (followedUser.follower[i] === followerId) {
-//         return { error: `You already following ${followedUser.nick_name}` };
-//       }
-//     }
-//     if (!followedUser)
-//       return { error: `User ${followedUser.nick_name} does not exist` };
-//     if (!followerUser)
-//       return { error: `User ${followerUser.nick_name} does not exist` };
+    for (let i = 0; i < followedUser.follower.length; i++) {
+      if (followedUser.follower[i] === followerId) {
+        return { error: `You already following ${followedUser.nick_name}` };
+      }
+    }
+    if (!followedUser)
+      return { error: `User ${followedUser.nick_name} does not exist` };
+    if (!followerUser)
+      return { error: `User ${followerUser.nick_name} does not exist` };
 
-//     followedUser.follower.push(followerId);
-//     followerUser.following.push(followedId);
+    followedUser.follower.push(followerId);
+    followerUser.following.push(followedId);
 
-//     followedUser.save();
-//     followerUser.save();
+    followedUser.save();
+    followerUser.save();
 
-//     return { message: `You are now following ${followedUser.nick_name}` };
-//   } catch (error) {}
-// }
+    return { message: `You are now following ${followedUser.nick_name}` };
+  } catch (error) {}
+};
+
 module.exports = ReactService;
