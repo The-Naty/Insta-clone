50c683a4712953774c071618ee17cc9c6e83e8b3
The-Naty <youssef-el-zanaty@outlook.com>
2022-09-21 05:47:55 +0200

    Update auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const AuthController = require("../Controllers/auth");
 const RequestValidator = require("../Middlewares/requestValidator");
 const UserValidator = require("../Validations/createUser");
-const auth = require("../Middlewares/auth");
 
 router.post(
   "/signup",
