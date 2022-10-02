6cd7f7259cda75fef4db0189b47ee72d9e9d44b7
The-Naty <youssef-el-zanaty@outlook.com>
2022-10-02 19:55:05 +0200

    user-interactions: created comment model and route

---
+++ Models/comment.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    owner_id: {
+      type: String,
+      required: true,
+    },
+    owner_name: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
--- Routes/react.js
+++ Routes/react.js
@@ -6,5 +6,6 @@ const auth = require("../Middlewares/auth");
 router.put("/follow", auth, ReactController.followUser);
 router.put("/unfollow", auth, ReactController.unFollowUser);
 router.put("/likePost", auth, ReactController.likePost);
+router.post("/comment", auth, ReactController.commentPost);
 
 module.exports = router;
